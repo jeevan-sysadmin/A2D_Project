3edd3d986ff67cdfa9b11fd5494a24613af49e55
Jeevan larosh <jeevan2k3@gmail.com>
2024-11-05 00:16:01 +0530

    Home Page with 3D Elements and Form

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ThreeDModel from '../components/ThreeDModel';
+import Form from '../components/Form';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the 3D Website</h1>
+      <ThreeDModel />
+      <Form />
+    </div>
+  );
+};
+
+export default HomePage;

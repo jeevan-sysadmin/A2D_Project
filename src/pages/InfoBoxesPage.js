cfc94a2ffced28b5c9630d1dc8ae674b9ae29e89
Jeevan larosh <jeevan2k3@gmail.com>
2024-11-05 20:36:25 +0530

    box

--- src/pages/InfoBoxesPage.js
+++ src/pages/InfoBoxesPage.js
@@ -1,32 +1,38 @@
 // src/pages/InfoBoxesPage.js
 import React from 'react';
-import './InfoBoxesPage.css'; // Import CSS for styling
+import './InfoBoxesPage.css';
+import warrantyLogo from '../assets/logo/1year.png'; // Import the warranty logo
+import Hours from '../assets/logo/48.png'; // Import the hours logo
+import order from '../assets/logo/order.png'; // Import the order logo
+import free from '../assets/logo/free.png'; // Import the order logo
+
+
+
 
 const InfoBoxesPage = () => {
   return (
-  
-      
-      
-      <div className="info-boxes-container">
-        <div className="info-box">
-          <h2>On Site Upto</h2>
-          <h1>1 Year</h1>
-          <p> Warranty</p>
-        </div>
-        <div className="info-box">
-          <h2>In-House</h2>
-          <p>48 Hours Testing</p>
-        </div>
-        <div className="info-box">
-          <h2>Know The</h2>
-          <p>Performance</p>
-        </div>
-        <div className="info-box">
-          <h2>Pan India</h2>
-          <p>Free Delivery</p>
-        </div>
+    <div className="info-boxes-container">
+      <div className="info-box">
+      <img src={warrantyLogo} alt="1 Year Warranty Logo" className="warranty-logo" />
+        <h2>On Site Upto 1 Year</h2>
+        <h3>Warranty</h3>
       </div>
-    
+      <div className="info-box">
+      <img src={Hours} alt="1 Year Hours Logo" className="warranty-logo" />
+        <h2>In-House 48 Hours</h2>
+        <h3>Testing</h3>
+      </div>
+      <div className="info-box">
+      <img src={order} alt="1 Year order Logo" className="warranty-logo" />
+        <h2>Know The Performance</h2>
+        <h3>Place the order</h3>
+      </div>
+      <div className="info-box">
+      <img src={free} alt="1 Year free Logo" className="warranty-logo" />
+        <h2>Pan India Free</h2>
+        <h3>Delivery</h3>
+      </div>
+    </div>
   );
 };
 

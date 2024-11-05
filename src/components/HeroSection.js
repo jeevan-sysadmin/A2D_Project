80abab1aa3b7e6ad5c54c4f6541fd771f983894d
Jeevan larosh <jeevan2k3@gmail.com>
2024-11-05 19:46:58 +0530

    Home page

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './HeroSection.css';
 import gif from '../assets/video/pc_3d.931dbb3564787a5552ac.gif'; 
+import logo1 from '../assets/logo/a2d.jpeg'; // Import the logo
 import Form from '../components/Form';
 import Modal from '../components/Modal';
 
@@ -17,6 +18,9 @@ const HeroSection = () => {
 
   return (
     <section className="hero-section">
+      <div className="logo1-container">
+        <img src={logo1} alt="Logo" className="logo1" />
+      </div>
       <div className="text-content">
         <h1 className="main-text">
           WE BUILD <span className="highlight-people">PEOPLE</span><br /> BEFORE WE <br /> BUILD <span className="highlight-pc">PC</span>
@@ -32,6 +36,11 @@ const HeroSection = () => {
       <Modal isOpen={isModalOpen} onClose={closeModal}>
         <Form />
       </Modal>
+      <div className="footer-quote">
+        If you want to <span className="highlight-quote">CRACK</span> the system, First <span className="highlight-quote">UNDERSTAND</span> the system
+        <br />
+        <span className="author">-PC Doc</span>
+      </div>
     </section>
   );
 };
--- src/pages/InfoBoxesPage.js
+++ src/pages/InfoBoxesPage.js
@@ -4,23 +4,14 @@ import './InfoBoxesPage.css'; // Import CSS for styling
 
 const InfoBoxesPage = () => {
   return (
-    <div className="info-boxes-page">
-      <h1 className="page-title">--------@ A2D PC Factory -------------</h1>
-      <h2 className="subtitle">How Things Work</h2>
-      <h3 className="customer-persona-title">BUILDING CUSTOMER PERSONA</h3>
-      <p className="customer-persona-description">
-        Building a customer persona is an essential step for our sales team to
-        provide tailored PC solutions. A customer persona is a detailed
-        representation of our ideal customer based on real data and insights
-        gathered through direct interactions. By developing a comprehensive 
-        understanding of our customers, we can better address their specific 
-        needs, preferences, and pain points.
-      </p>
+  
+      
       
       <div className="info-boxes-container">
         <div className="info-box">
           <h2>On Site Upto</h2>
-          <p>1 Year Warranty</p>
+          <h1>1 Year</h1>
+          <p> Warranty</p>
         </div>
         <div className="info-box">
           <h2>In-House</h2>
@@ -35,7 +26,7 @@ const InfoBoxesPage = () => {
           <p>Free Delivery</p>
         </div>
       </div>
-    </div>
+    
   );
 };
 
